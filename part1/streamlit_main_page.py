64ed44045f5a148bfee724b1761fa5e3dc737b79
MaelTriquet <mael1triquet@gmail.com>
2024-03-24 18:29:39 +0100

    css start

--- part1/streamlit_main_page.py
+++ part1/streamlit_main_page.py
@@ -1,26 +1,31 @@
 import streamlit as st
-from main import process_desciption
-# Define your Python function that will be executed when the button is clicked
-def process_input(input_text):
-    # Process the input here (e.g., print it)
-    return ("Input:", input_text)
-
-# Streamlit app layout
-st.title("Search Bar and Button Example")
-
-# Create a search bar
-search_input = st.text_input("Enter your search query:")
 
 session_state = st.session_state
-if "output" not in session_state:
-    session_state.output = None
+if "current_page" not in session_state:
+    session_state.current_page = "page_main"
 
 
-# Create a button to trigger the processing function
-if st.button("Process"):
-    # Call the processing function with the content of the search bar as an argument
-    output = process_desciption(search_input)
-    session_state.output = output
+def load_css():
+    try:
+        with open("part1/styles.css", "r") as f:
+            css = f.read()
+        st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+    except:
+        print("error")
 
-if session_state.output is not None:
-    st.write("Output:", session_state.output)
+# Load custom CSS styles
+load_css()
+
+st.markdown("""<div class="navbar">
+    <div class="dropdown">
+        <button class="dropbtn">Home</button>
+        <div class="dropdown-content">
+            <a href="#">Option 1</a>
+            <a href="#">Option 2</a>
+            <a href="#">Option 3</a>
+        </div>
+    </div>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#" class="navbar-right">Login</a>
+</div>""", unsafe_allow_html=True)
