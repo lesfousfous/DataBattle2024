bab84852e78bc0d33747ece8a931658d76cb0251
Nicolas Levrard <nicolaslevrard@gmail.com>
2024-03-24 21:42:23 -0400

    gain financer et gain energie

---
+++ part2/main.py
@@ -0,0 +1,294 @@
+from matplotlib import pyplot as plt
+import mysql.connector
+from mysql.connector import MySQLConnection
+from configparser import ConfigParser
+import numpy as np
+from sklearn.linear_model import LinearRegression 
+
+
+class Database:
+
+    def __init__(self) -> None:
+        config = ConfigParser()
+        config.read("config.ini")
+        self.database_connection = Database._init_database_connection(config)
+
+    def _init_database_connection(config_file: ConfigParser) -> MySQLConnection:
+        mydb = mysql.connector.connect(
+            host=config_file['mysqlDB']['host'],
+            user=config_file['mysqlDB']['user'],
+            password=config_file['mysqlDB']["password"],
+            database=config_file['mysqlDB']['db'],
+        )
+        return mydb
+
+class DatabaseObject:
+    db = Database()
+    cursor = db.database_connection.cursor()
+
+
+class QueryDb(DatabaseObject):
+
+    def get_gain_and_cout_financier(self, codeSolution: int):
+
+        # | numgainrex | codesolution | coderex | gainfinanciergainrex | codemonnaiegainrex | codeperiodeeconomie | energiegainrex | uniteenergiegainrex | codeperiodeenergie | gesgainrex | minigainrex | maxigainrex | moyengainrex | reelgainrex | trireelgainrex | trimingainrex | trimaxgainrex | codelicense | numcoutrex | codesolution | coderex | minicoutrex | maxicoutrex | reelcoutrex | codemonnaiecoutrex | codeunitecoutrex | codedifficulte | codelicense |
+        DatabaseObject.cursor.execute(
+            f"""select g.coderex, gainfinanciergainrex, codemonnaiegainrex,codeperiodeeconomie , reelcoutrex, codemonnaiecoutrex
+            from tblgainrex as g 
+            join tblcoutrex as c 
+            on c.coderex = g.coderex and c.codesolution = g.codesolution 
+            where   g.codesolution = {codeSolution}
+                    and not gainfinanciergainrex is null
+                    and not c.reelcoutrex is null
+                    and codeperiodeeconomie > 2
+            """
+        )
+        return DatabaseObject.cursor.fetchall()
+
+
+    def get_gainenergie(self, codeSolution: int):
+        # energiegainrex | reelcoutrex | uniteenergiegainrex | codeperiodeenergie | codemonnaiecoutrex
+        DatabaseObject.cursor.execute(
+            f"""
+            SELECT g.energiegainrex,
+                c.reelcoutrex,
+                g.uniteenergiegainrex,
+                g.codeperiodeenergie,
+                c.codemonnaiecoutrex
+            FROM tblgainrex as g
+            INNER JOIN tblcoutrex as c
+            ON g.coderex = c.coderex
+            and c.codesolution = g.codesolution
+            WHERE g.codesolution = {codeSolution}
+                AND NOT g.energiegainrex IS NULL
+                AND NOT g.uniteenergiegainrex IS NULL
+                AND NOT g.codeperiodeenergie IS NULL
+                AND NOT c.reelcoutrex IS NULL
+                AND NOT c.codemonnaiecoutrex IS NULL
+                AND g.codeperiodeenergie > 2
+            """
+        )
+        return DatabaseObject.cursor.fetchall()
+    
+
+
+
+class Convert(DatabaseObject):
+
+
+# Sauvegarder toute la table en mémoire car petite table et evite plusieur requetes
+    @staticmethod
+    def to_euro(value : float, codeMonnaie:int = 2):
+        DatabaseObject.cursor.execute(
+        f"""SELECT valeurtauxmonnaie FROM tbltauxmonnaie WHERE codemonnaie = {codeMonnaie}"""
+        )
+
+        res = DatabaseObject.cursor.fetchall()
+
+        taux = 1
+        if res and len(res) == 1:
+            taux = float(res[0][0])
+        else:
+            raise Exception("Code monnaie inconnue")
+        
+        return value * taux
+
+
+    @staticmethod
+    def to_year(value: float, codePeriode: int = 1):
+
+        # if codePeriode == 1:
+        #     raise Exception("Unité manquante")
+        
+        if codePeriode == 2:
+            raise Exception("/Projet ne peut etre transformé en /an")
+
+        if codePeriode > 5:
+            raise Exception("Pas traité")
+
+        if codePeriode == 5: # passage de /heure => /jour
+            value *= 24
+            codePeriode = codePeriode - 1
+
+        if codePeriode == 4: # passage de /jour => /an
+            value *= 365.25
+            codePeriode = codePeriode - 1
+        
+        return value
+
+
+    @staticmethod
+    def unit_convertion(value: float, codeUnit:int):
+
+
+        DatabaseObject.cursor.execute(
+        f"""
+        SELECT traductiondictionnairecategories
+        FROM tbldictionnairecategories
+        WHERE codelangue = 2
+        AND typedictionnairecategories = "uni"
+        AND codeappelobjet = {codeUnit}
+        """
+        )
+
+        res = DatabaseObject.cursor.fetchall()
+        unit = ""
+        if res and len(res) == 1:
+            unit = res[0][0] #.split(" ")[0] # si osef de cunac
+        else:
+            raise Exception("Code monnaie inconnue")
+        
+        conversions = {
+            'GJ': {
+                'kWh': 277.778
+                # Pour le gaz naturel, la conversion en mètre cube (m3) dépend du pouvoir calorifique du gaz.
+            },
+            'tep': {
+                'GJ': 41.868,
+            },
+            'GWh': {
+                'kWh': 1000000,
+            },
+            'MMBtu': {
+                'GJ': 1.05506,
+            },
+            'm3': {
+                'litre': 1000,
+                # Pour le gaz naturel, la conversion en GJ dépend du pouvoir calorifique du gaz.
+            },
+            'therm': {
+                'MMBtu': 0.1055,
+            },
+            'tonne': {
+                'kg': 1000,
+            },
+            'W': {
+                'kW': 0.001,
+            },
+            'MWh': {
+                'kWh': 1000,
+            }
+        }
+
+        keys = list(conversions.keys())
+        while(unit in keys):
+            newUnit = next(iter(conversions[unit]))
+            value = value * float(conversions[unit][newUnit])
+            unit = newUnit
+
+        return value, unit
+
+
+class Parse(QueryDb):
+
+    def parse_gain_financer(self, codeSolution: int):
+        # (coderex, gainfinanciergainrex, codemonnaiegainrex, codeperiodeeconomie, reelcoutrex, codemonnaiecoutre)
+        outputRexFin = self.get_gain_and_cout_financier(codeSolution)
+
+        res = []
+        for rex in outputRexFin:
+            codeMonnaieGain = rex[2] if rex[2] != 1 else rex[5]
+            if codeMonnaieGain == 1:
+                continue
+
+            gainEuro = Convert.to_euro(float(rex[1]), codeMonnaieGain)
+            gainEuro = Convert.to_year(gainEuro, rex[3])
+
+            codeMonnaieCout = rex[5] if rex[5] != 1 else rex[2]
+            coutEuro = Convert.to_euro(float(rex[4]), codeMonnaieCout)
+
+
+            res.append((rex[0], coutEuro, gainEuro))
+        
+        return res
+    
+    def parse_gain_eco(self, codeSolution:int):
+        # (energiegainrex | reelcoutrex | uniteenergiegainrex | codeperiodeenergie | codemonnaiecoutrex)
+        outputRexEco = self.get_gainenergie(codeSolution)
+
+        res = []
+        for rex in outputRexEco:
+            codeMonnaieGain = rex[4] 
+            if codeMonnaieGain == 1:
+                continue
+
+            gainEuro = Convert.to_euro(float(rex[1]), codeMonnaieGain)
+
+            gainEnergie, unit = Convert.unit_convertion(float(rex[0]), rex[2])
+            gainEnergie = Convert.to_year(gainEnergie, rex[3])  
+
+            res.append((gainEuro, gainEnergie, unit))
+
+        return res
+
+    
+def regression_lineaire(X: np.array, Y:np.array):
+    
+    model = LinearRegression()
+    model.fit(X, Y)
+    
+    return model
+
+def show_regression(model, X, Y, xlabel: str, ylabel: str):
+    # Prédire les valeurs y pour les données d'entraînement
+    y_pred = model.predict(X)
+
+    # Tracer les données et la courbe de régression linéaire
+    plt.scatter(X, Y, color='blue', label='Données')
+    plt.plot(X, y_pred, color='red', linewidth=1, label='Régression linéaire')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title('Régression linéaire')
+    # plt.xscale('log')  # Échelle logarithmique pour l'axe des x
+    # plt.yscale('log')  # Échelle logarithmique pour l'axe des y
+    plt.legend()
+    plt.show()
+
+
+def gainFinancier(codeSolution: int):
+    parse = Parse()
+
+    res = parse.parse_gain_financer(codeSolution)
+    if len(res) == 0:
+        print("aucune valeur")
+        exit(402)
+
+    X = []
+    Y = []
+    for _rex, cout, gain in res:
+        X.append(gain)
+        Y.append(cout)
+
+    X = np.array(X).reshape(-1,1)
+    Y = np.array(Y)    
+
+    model = regression_lineaire(X, Y)
+    show_regression(model, X, Y, "gain €/an", "cout €") 
+    pass
+
+
+
+if __name__ == '__main__':
+    db = QueryDb()
+    #print(db.get_gainfinancier(79))
+    parse = Parse()
+
+    data = parse.parse_gain_eco(160)
+    print(data)
+
+    if len(data) == 0:
+        print("aucune valeur")
+        exit(402)
+
+    X = []
+    Y = []
+    for cout, gain, unit in data:
+        X.append(gain)
+        Y.append(cout)
+
+    X = np.array(X).reshape(-1,1)
+    Y = np.array(Y)  
+
+    model = regression_lineaire(X, Y)
+    show_regression(model, X, Y, f"{data[0][2]}/an", "cout €")
+    
